88488a19d3b61b06ca4fd923bd13115f3b9cdb51
Geenius-code <Ranjeetageenius@gmail.com>
2024-07-07 11:51:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function welcome() {
+	window.open("https://www.youtube.com/")
+}
+function windowOpen() {
+	window.open("https://www.google.com/")
+}
+function alert() {
+	window.open("https://www.chatgpt.com/")
+}
